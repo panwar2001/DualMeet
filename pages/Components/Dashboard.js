b64ebe02b3050b2977dca832ca4a6a5ed26f7160
Ayush panwar <ayush.s.panwar2001@gmail.com>
2023-05-19 22:20:21 +0530

    Made Dashboard responsive

--- pages/Components/Dashboard.js
+++ pages/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import Header from './Header'
+import NavigationDashboard from './navigation/NavigationDashboard'
 import Image from 'next/image';
 import ImageSlider from './ImageSlider';
 import {useRouter} from 'next/router'; 
@@ -16,32 +16,79 @@ export default function Dashboard() {
   const joinNewMeeting=()=>{
     router.push(`/join/${MeetingId.trim()}`)
   }
+  const styleMeetingText={
+    fontSize:'2.7em',
+    lineHeight:'1'
+  }
+  const createNewMeetingButton={
+    backgroundColor:'#1a73e8',
+    color:'#fff',
+    fontWeight:'bold',
+    cursor:'pointer',
+    border:'2px solid #eee',
+    fontSize:'20px'
+  }
+  const joinButton={
+  fontSize:'1.5em',
+  backgroundColor:'transparent',
+  color:'blue',
+  cursor:'pointer'
+  }
     return (<>
-              <Header/>
-              <div style={{display:'flex'}}>
-              <div style={{paddingTop:'3%',marginLeft:'5%',width:'70%'}}>
-                <h1 style={{fontSize:'2.7em',lineHeight:'.5'}}>
-                 Enjoy top-tier video meetings 
+              <NavigationDashboard/>
+              <div className='sliderResponsive'>
+                <div style={{paddingTop:'3%',marginLeft:'5%',width:'70%'}}>
+                <h1 style={styleMeetingText}>
+                 Enjoy top-tier video meetings <br/>at no cost.
                 </h1>
-                <h1 style={{fontSize:'2.7em',lineHeight:'.5'}}>
-                 at no cost.
-                 </h1>
-                <p style={{color:'grey'}}>Designed  secure business meeting service, DualMeet<br/> to make it free and accessible to everyone. <br/></p>
-                <div style={{display:'flex',paddingTop:'5%'}}>
+                <p >Designed  secure business meeting service, DualMeet<br/> to make it free and accessible to everyone. <br/></p>
+                <div className="CreateOrJoin">
                  <div>
-                  <button  onClick={createNewMeeting} style={{height:'40px',backgroundColor:'#1a73e8',color:'#fff',fontWeight:'bold',cursor:'pointer',border:'2px solid #eee',fontSize:'20px',display:'flex'}}><Image src='/cam.svg' alt='Brand logo' height={30} width={40}/> new meeting</button>
+                  <button  onClick={createNewMeeting} style={createNewMeetingButton}><table><tr><td><Image src='/cam.svg' alt='Brand logo' height={30} width={40}/></td><td> new meeting</td></tr></table></button>
                 </div>
+                &nbsp;&nbsp;
                 <div>
-                 or 
-                <input type='text' name="join" onChange={(e)=>setMeetingId(e.target.value)} placeholder='Enter a code or link' style={{fontSize:'1.3em'}}/>
-                <button onClick={joinNewMeeting} type='button' style={{fontSize:'1.3em',backgroundColor:'transparent',color:'blue',cursor:'pointer '}}> Join</button>
+                <input type='text' name="join" onChange={(e)=>setMeetingId(e.target.value)} placeholder='Enter a code or link' />
+                <button onClick={joinNewMeeting} type='button' style={joinButton}> Join</button>
                  </div>
                 </div>
               </div>
-              <div style={{paddingTop:'5%',paddingRight:'4%'}}>
+              <div className="slider" >
               <ImageSlider style={{paddingLeft:'50%'}}/>
               </div>
               </div>
+              <style jsx>{`
+                .sliderResponsive{
+                  display:flex;
+                }
+                .CreateOrJoin{
+                  display:flex;
+                  padding-top:5%;
+                }
+              @media(max-width:960px){
+                .sliderResponsive{
+                  display:block;
+                  text-align:center;
+                  align-content:center;
+                  justify-content:center;
+                }
+                .CreateOrJoin{
+                  display:block;
+                  padding-top:5%;
+                }
+              }
+              .slider{
+                padding-top:5%;
+                padding-right:4%;
+              }
+              p{
+                color:grey;
+              }
+              input{
+               font-size:1.5em;
+               width:60%;
+              }
+              `}</style>
       </>
     )
   }
